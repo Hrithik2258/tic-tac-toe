a21fda5ed5e0f530c71f10e5c56e08f36f1d8e52
Hrihtik Pawar <86373306+Hrithik2258@users.noreply.github.com>
2022-05-19 17:53:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ import React from "react";
+import Board from "./components/Board";
+
+function App() {
+  return (
+<div className="App">
+  <h1 className="header">Tic tac Toe   </h1>
+
+  <Board/>
+
+  </div>
+
+      );
+}
+
+export default App;
